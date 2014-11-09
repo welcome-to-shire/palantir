6d5104f7a2bde2d6139c9480cde346c6201a3247
hbc <bcxxxxxx@gmail.com>
2014-11-09 15:53:54 +0800

    Bootstrap android workspace.

---
+++ clients/android/src/shire/bcho/palantir/PalantirActivity.java
@@ -0,0 +1,15 @@
+package shire.bcho.palantir;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PalantirActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

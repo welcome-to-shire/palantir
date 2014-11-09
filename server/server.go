70815aca169986db4fdf8001b9538c5210361dd9
hbc <bcxxxxxx@gmail.com>
2014-11-09 15:41:32 +0800

    Refactor server instance.

--- server/server.go
+++ server/server.go
@@ -2,28 +2,59 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	setupStorage()
+type Server struct {
+	Host string
+	Port uint
 
-	log.Fatal(http.ListenAndServe(":8080", setupServer()))
+	router *mux.Router
 }
 
-func setupStorage() {
-	GetStorageInstance()
+func (s Server) Bind() string {
+	return fmt.Sprintf("%s:%d", s.Host, s.Port)
 }
 
-func setupServer() *mux.Router {
+func (s *Server) Setup() {
 	r := mux.NewRouter()
 	ns := r.PathPrefix("/api/v1").Subrouter()
 
 	ns.HandleFunc("/{subject}", getNotification).Methods("GET")
 	ns.HandleFunc("/{subject}", createNotification).Methods("POST")
 
-	return r
+	s.router = r
+}
+
+func (s Server) Start() {
+	log.Printf("server start listening at: %s", s.Bind())
+	log.Fatal(http.ListenAndServe(s.Bind(), s.router))
+}
+
+func main() {
+	var server Server
+
+	flag.StringVar(&server.Host, "host", "127.0.0.1", "bind host")
+	flag.UintVar(&server.Port, "port", 9092, "bind port")
+	flag.Parse()
+
+	setupLogger()
+	setupStorage()
+
+	server.Setup()
+	server.Start()
+}
+
+func setupStorage() {
+	GetStorageInstance()
+}
+
+func setupLogger() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("Palantír ")
 }
